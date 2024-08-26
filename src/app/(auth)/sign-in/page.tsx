2dd94d18989b880f067921de345d3feaf1551db7
Muhammad Saad Ahmed <147624130+saadahmedacademy@users.noreply.github.com>
2024-08-27 00:28:53 +1200

    Update page.tsx

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -85,7 +85,7 @@ export default function SignUpForm() {
   
   return (
     <div className="flex justify-center items-center min-h-screen bg-blue-700">
-      <div className="w-full max-w-md p-8 bg-white rounded-lg space-y-2 shadow-lg ">
+      <div className="lg:w-full sm:max-w-md p-8 bg-white rounded-lg space-y-2 shadow-lg ">
         <div className="text-center">
           <p className="mb-4">Sign In to get started</p>
         </div>
